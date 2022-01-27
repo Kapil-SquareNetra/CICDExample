4392182f392c9906eb3d23690cd5b80717bc035b
kapil <kapleshwar.7925@gmail.com>
2022-01-27 19:00:38 +0530

    added feature 1

--- app/src/main/java/com/example/cicdexample/MainActivity.kt
+++ app/src/main/java/com/example/cicdexample/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.cicdexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nameEditTextView: EditText
+    private lateinit var nameText: TextView
+    lateinit var saveButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nameEditTextView = findViewById(R.id.name_view)
+        nameText = findViewById(R.id.nameText)
+        saveButton = findViewById(R.id.saveBtn)
+
+        saveButton.setOnClickListener {
+            if (nameEditTextView.text.isNotBlank())
+                nameText.text = nameEditTextView.text
+        }
+
     }
 }
